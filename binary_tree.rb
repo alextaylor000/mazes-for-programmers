40a961c7df00407f0147ab629cc0486fc9bd07b2
Alex Taylor <alexmctaylorpants@gmail.com>
2016-10-16 10:47:53 -0700

    Make grid bigger; use `sample` instead of `rand`

--- binary_tree.rb
+++ binary_tree.rb
@@ -2,11 +2,11 @@ class BinaryTree
   def self.on(grid)
     grid.each_cell do |cell|
       neighbours = []
+
       neighbours << cell.north if cell.north
       neighbours << cell.east if cell.east
 
-      index = rand(neighbours.length)
-      neighbour = neighbours[index]
+      neighbour = neighbours.sample
 
       cell.link(neighbour) if neighbour
     end
